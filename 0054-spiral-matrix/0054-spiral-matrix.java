352bbe3cac282220a66f1b6fdd8d48d7c6375d0a
Mohamed Irfan <mohamedirfan181105@gmail.com>
2026-02-11 22:13:01 +0530

    Time: 0 ms (100%), Space: 43.1 MB (59.22%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.ArrayList;
 class Solution 
 {
     public List<Integer> spiralOrder(int[][] matrix) 
@@ -8,44 +8,43 @@ class Solution
 
         int left =0;
         int top =0;
-        int right = m-1;
-        int bottom = n-1;
+        int right =m-1;
+        int bottom =n-1;
 
-        List<Integer> list = new ArrayList<>();
-
-        while(top <= bottom && left <= right)
-
-        {
-          for(int i=left; i<=right; i++)
-        {
-           list.add(matrix[top][i]);
-        }
-        top++;
-
-        for(int i=top; i<=bottom; i++)
-        {
-           list.add(matrix[i][right]);
-        }
-        right--;
-      
-        if(top <= bottom)
-        {
-        for(int i=right; i>=left; i--)
-        {
-          list.add(matrix[bottom][i]);
-        }
-        bottom--;
-        }
+        ArrayList<Integer> list = new ArrayList<>();
         
-        if(left <= right)
+        while(left <= right && top <= bottom)
         {
-        for(int i=bottom; i>=top; i--)
-        {
-           list.add(matrix[i][left]);
+           for(int i=left; i<=right; i++)
+           {
+             list.add(matrix[top][i]);
+           }
+           top++;
+
+           for(int i=top; i<=bottom; i++)
+           {
+             list.add(matrix[i][right]);
+           }
+           right--;
+
+           if(top <= bottom)
+           {
+           for(int i=right; i>=left; i--)
+           {
+             list.add(matrix[bottom][i]);
+           }
+            bottom--;
+           }
+
+           if(left <= right)
+           {
+             for(int i=bottom; i>=top; i--)
+             {
+               list.add(matrix[i][left]);
+             }
+              left++;
+           }
         }
-        left++;
-        }
-        }
-        return list;
+          return list;
     }
 }
