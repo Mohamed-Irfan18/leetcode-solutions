0a350c6655bdcd30f2e4ee6a4a96f7fcee3ae6b4
Mohamed Irfan <mohamedirfan181105@gmail.com>
2026-08-17 14:42:22 +0530

    Time: 16 ms (4.76%), Space: 56.7 MB (9.05%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,33 +3,22 @@ class Solution
     public int singleNonDuplicate(int[] nums) 
     {
         int len = nums.length;
-        if(len == 1)
-        return nums[0];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<len; i++)
         {
-            if(i == 0)
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+
+        for(int key : map.keySet())
+        {
+            if(map.get(key) == 1)
             {
-                if(nums[i] != nums[i+1])
-                {
-                    return nums[i];
-                }
-            }
-            else if(i == len-1)
-            {
-                if(nums[i] != nums[i-1])
-                {
-                    return nums[i];
-                }
-            }
-            else
-            {
-                if(nums[i] != nums[i+1] && nums[i] != nums[i-1])
-                {
-                    return nums[i];
-                }
+                return key;
             }
         }
-        return nums[0];
+
+        return -1;
     }
 }
