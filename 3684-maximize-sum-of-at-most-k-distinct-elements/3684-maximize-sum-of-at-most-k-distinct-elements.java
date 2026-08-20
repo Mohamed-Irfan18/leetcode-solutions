9913b155318b20831c3f44817e84e65465ed3634
Mohamed Irfan <mohamedirfan181105@gmail.com>
2026-08-20 15:58:35 +0530

    Time: 6 ms (97.74%), Space: 47.4 MB (86.83%) - LeetHub

--- 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.java
+++ 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.java
@@ -2,23 +2,10 @@ class Solution
 {
     public int[] maxKDistinct(int[] nums, int k) 
     {
-        int len = nums.length;
-
-        for(int i=0; i<len; i++)
-        {
-            for(int j=i+1; j<len; j++)
-            {
-                if(nums[j] < nums[i])
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-        }
+        mergeSort(nums,0, nums.length-1);
 
         LinkedHashSet<Integer> set = new LinkedHashSet<>();
-
+        
         for(int i=nums.length-1; i>=0; i--)
         {
             if(set.size() < k)
@@ -28,12 +15,64 @@ class Solution
         }
 
         int[] res = new int[set.size()];
-        int ind =0;
+        int index = 0;
 
         for(int t : set)
         {
-            res[ind++] = t;
+            res[index++] = t;
         }
+
         return res;
+    }
+
+    public static void mergeSort(int[] nums, int left, int right)
+    {
+         if(left >= right)
+        {
+            return;
+        }
+
+        int mid = left + (right-left)/2;
+
+        mergeSort(nums,left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        merge(nums, left, mid, right);
+    }
+
+    public static void merge(int[] nums, int left, int mid, int right)
+    {
+        int[] temp = new int[right-left+1];
+        int ind = 0;
+
+        int i =left;
+        int j =mid+1;
+
+        while(i<=mid && j<=right)
+        {
+            if(nums[i] <= nums[j])
+            {
+                temp[ind++] = nums[i++];
+            }
+            else
+            {
+                temp[ind++] = nums[j++];
+            }
+        }
+
+        while(i<=mid)
+        {
+            temp[ind++] = nums[i++];
+        }
+
+        while(j<=right)
+        {
+            temp[ind++] = nums[j++];
+        }
+
+        for(int x=0; x<temp.length; x++)
+        {
+            nums[left+x] = temp[x];
+        }
     }
 }
